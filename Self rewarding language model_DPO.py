24f9977c53cfb34111e4271b100f2a02f5e8393f
rigvedb <122348418+rigvedb@users.noreply.github.com>
2024-01-29 01:30:29 +0530

    Update Self rewarding language model_DPO.py

--- Self rewarding language model_DPO.py
+++ Self rewarding language model_DPO.py
@@ -26,3 +26,18 @@ from pytorch_custom_utils.accelerate_utils import (
 )
 
 from torchtyping import TensorType
+
+
+
+def exists(v):
+    return v is not None
+
+def dreeze_all_layers_(module):
+    for param in module.parameters():
+        param.requires_grad = False #disabling gradient calculations for that parameters
+        
+        
+def log_prob_from_model_and_sequence(model,seq,eps=1e-20):
+    logits = model(seq)
+    prob = logits.softmax(dim = -1)
+    return get_at('b n [c], b n -> b n', prob,indices).clamp(min = eps).log()
