78a5eb9172229b82f590bba9a3be93188ebcfc4d
rigvedb <122348418+rigvedb@users.noreply.github.com>
2024-02-02 03:01:11 +0530

    Update SRLM_pytorch.py
    fine tuned the class
    used selfrewardtrainer class

--- SRLM_pytorch.py
+++ SRLM_pytorch.py
@@ -144,3 +144,101 @@ class RewardGenerator(Module):
 
     def forward(self) -> Dataset:
         raise NotImplementedError
+
+class SelfRewardingTrainer(Module):
+    @beartype
+    def __init__(
+        self,
+        model: Module,
+        *,
+        train_sft_dataset: Optional[Union[List[Dataset], Dataset]] = None,
+        valid_sft_dataset: Optional[Dataset] = None,
+        beta = 0.1,
+        self_reward_num_iterations = 2,
+        reward_prompt_config: dict = REWARD_PROMPT_CONFIG,
+        reward_iteration_type = ['default', 'default'],
+        num_preference_pairs: List[int] = [3964, 6942],
+        reward_generator_kwargs: dict = dict(
+            num_candidate_responses = 4,
+            temperature = 0.7,
+            nucleus_p = 0.9,
+        ),
+        model_generate_prompt: Optional[Module] = None,
+        early_stopper: Optional[EarlyStopper] = None,
+        accelerate_kwargs: dict = dict(),
+        sft_trainer_kwargs: dict = dict(),
+        dpo_trainer_kwargs: dict = dict(),
+        dropout = 0.1,
+        checkpoints_folder: str = './checkpoints'
+    ):
+        super().__init__()
+        assert all([key in reward_prompt_config for key in reward_iteration_type]), f'reward prompt must be one of {reward_prompt_config.keys()}'
+
+        if not exists(model_generate_prompt):
+            model_generate_prompt = deepcopy(model)
+
+        self.reward_prompt_configs = [reward_prompt_config[key] for key in reward_iteration_type]
+        self.self_reward_num_iterations = self_reward_num_iterations
+
+        self.model = model
+        self.first_iterate_on_sft = exists(train_sft_dataset)
+
+        self.accelerator = Accelerator(**accelerate_kwargs)
+
+        self.sft_trainer = None
+        if self.first_iterate_on_sft:
+            self.sft_trainer = SFTTrainer(
+                model,
+                accelerator = self.accelerator,
+                train_dataset = train_sft_dataset,
+                valid_dataset = valid_sft_dataset,
+                **sft_trainer_kwargs
+            )
+
+        self.reward_generators = [RewardGenerator(model = model, model_generate_prompt = model_generate_prompt, reward_config = reward_config, num_preference_pairs = one_stage_num_preference_pairs) for reward_config, one_stage_num_preference_pairs in zip(self.reward_prompt_configs, num_preference_pairs)]
+
+        set_dropout_(model, dropout)
+        self.dpo = DPO(model)
+
+        self.dpo_trainer = DPOTrainer(
+            dpo = self.dpo,
+            accelerator = self.accelerator,
+            **dpo_trainer_kwargs
+        )
+
+        checkpoints_folder = Path(checkpoints_folder)
+        checkpoints_folder.mkdir(parents = True, exist_ok = True)
+        self.checkpoints_folder = checkpoints_folder
+
+    def save(self, path: str, overwrite: bool = False):
+        if not self.accelerator.is_main_process:
+            return
+
+        path = self.checkpoints_folder / path
+
+        assert not path.exists() or overwrite, f'file already exists'
+
+        pkg = dict(
+            model = self.model.state_dict()
+        )
+
+        torch.save(pkg, str(path))
+
+    def forward(self):
+
+        if self.first_iterate_on_sft:
+            self.sft_trainer()
+            self.save('sft.ckpt.pt')
+
+        for ind, reward_generator in enumerate(range(self.reward_generators)):
+            iterate_num = ind + 1
+
+            self_reward_dataset = reward_generator()
+
+            self.dpo_trainer(self_reward_dataset)
+
+            self.dpo.update_reference_model_with_policy()
+
+            self.save(f'self-reward.{iterate_num}.ckpt.pt')
+
+        print(f'done')
